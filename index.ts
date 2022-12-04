1d2c0cc4ca85efb866c310918a609ed4d86eea2f
david <david@david.david>
2022-12-04 02:02:11 +0200

    feat: adding buttons

--- index.ts
+++ index.ts
@@ -1,91 +1,25 @@
+import { Button } from "./button";
 import { Audio } from "./audio";
-import { save } from "./save";
+import { Recorder } from "./recorder";
 
 let recorder: Recorder;
 
-document.addEventListener("keyup", async ({ code }) => {
-  console.log(code);
-  if (code === "Enter") {
-    getBase64ReadVoice();
+const buttonsContainer = document.createElement("div");
+document.body.appendChild(buttonsContainer);
+const allAudioContainer = document.createElement("div");
+document.body.appendChild(allAudioContainer);
+
+new Button().init("Start", buttonsContainer, async () => {
+  if (!recorder) {
+    recorder = new Recorder();
   }
+
+  await recorder.start();
 });
-
-export async function getBase64ReadVoice() {
-  return new Promise(async () => {
-    if (!recorder) {
-      recorder = new Recorder();
-    }
-
-    const listener = async ({ code }: KeyboardEvent) => {
-      console.log(code);
-      if (code === "Space") {
-        const { base64Audio } = await recorder.stop();
-        console.log(base64Audio);
-        document.removeEventListener("keyup", listener);
-        document.body.appendChild(new Audio("audio", base64Audio).getElement());
-      }
-    };
-    document.addEventListener("keyup", listener);
-
-    await recorder.start();
-  });
-}
-
-class Recorder {
-  private _chunks: Blob[];
-  private _mediaRecorder: MediaRecorder;
-
-  async start() {
-    // https://stackoverflow.com/a/70665493/10198772
-    if (!this._chunks) {
-      await this.init();
-    }
-    this._chunks = [];
-    this._mediaRecorder.start(500);
-  }
-
-  async stop(): Promise<{ base64Audio: string }> {
-    const type = MediaRecorder.isTypeSupported("audio/ogg");
-    // const type = MediaRecorder.isTypeSupported("audio/webm")
-    //   ? "audio/webm"
-    //   : "audio/ogg";
-    const blob = new Blob(this._chunks, { type: "audio/ogg" });
-
-    this._chunks = [];
-    this._mediaRecorder.stop();
-
-    const base64Audio = await new Promise<string>((resolve) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result as string);
-      reader.readAsDataURL(blob);
-    });
-    return {
-      base64Audio,
-    };
-  }
-
-  private async init() {
-    const stream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
-      audio: {
-        noiseSuppression: false,
-        echoCancellation: false,
-        autoGainControl: false,
-      },
-    });
-    const track = stream.getAudioTracks()[0];
-    if (!track) throw "System audio not available";
-
-    stream.getVideoTracks().forEach((track) => track.stop());
-
-    const mediaStream = new MediaStream();
-    mediaStream.addTrack(track);
-
-    this._mediaRecorder = new MediaRecorder(mediaStream, {
-      audioBitsPerSecond: 5000000,
-    });
-    this._mediaRecorder.ondataavailable = (event) => {
-      if (event.data.size > 0) this._chunks.push(event.data);
-    };
-  }
-}
+new Button().init("Stop", buttonsContainer, async () => {
+  const { base64Audio } = await recorder.stop();
+  console.log(base64Audio);
+  const audioContainer = document.createElement("div");
+  audioContainer.appendChild(new Audio("audio", base64Audio).getElement());
+  allAudioContainer.appendChild(audioContainer);
+});
---
+++ recorder.ts
@@ -0,0 +1,58 @@
+export class Recorder {
+  private _chunks: Blob[];
+  private _mediaRecorder: MediaRecorder;
+
+  async start() {
+    // https://stackoverflow.com/a/70665493/10198772
+    if (!this._chunks) {
+      await this.init();
+    }
+    this._chunks = [];
+    this._mediaRecorder.start(500);
+  }
+
+  async stop(): Promise<{ base64Audio: string }> {
+    // const type = MediaRecorder.isTypeSupported("audio/wav");
+    // const type = MediaRecorder.isTypeSupported("audio/webm")
+    //   ? "audio/webm"
+    //   : "audio/ogg";
+    const blob = new Blob(this._chunks, { type: "audio/ogg" });
+
+    this._chunks = [];
+    this._mediaRecorder.stop();
+
+    const base64Audio = await new Promise<string>((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.readAsDataURL(blob);
+    });
+    return {
+      base64Audio,
+    };
+  }
+
+  private async init() {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: {
+        noiseSuppression: false,
+        echoCancellation: false,
+        autoGainControl: false,
+      },
+    });
+    const track = stream.getAudioTracks()[0];
+    if (!track) throw "System audio not available";
+
+    stream.getVideoTracks().forEach((track) => track.stop());
+
+    const mediaStream = new MediaStream();
+    mediaStream.addTrack(track);
+
+    this._mediaRecorder = new MediaRecorder(mediaStream, {
+      audioBitsPerSecond: 128000,
+    });
+    this._mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) this._chunks.push(event.data);
+    };
+  }
+}
